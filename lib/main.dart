ffa0343895b10b4fe33adca581fe69afa88b200e
Holy-Morphism <ranaadeeltahir@gmail.com>
2023-11-25 02:38:26 +0500

    go router added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kylipp/config/router/router.dart';
 import 'package:kylipp/config/theme/theme.dart';
-import 'package:kylipp/features/authentication/presentation/screens/log_in_screen.dart';
 
 void main() {
   runApp(const KYLIPP());
@@ -11,9 +11,9 @@ class KYLIPP extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: theme,
-      home: const LogInScreen(),
+      routerConfig: router,
     );
   }
 }
