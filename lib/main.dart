b4f3d8a184833b13d2ba74312dae89a10024f980
Holy-Morphism <ranaadeeltahir@gmail.com>
2023-11-25 13:55:37 +0500

    injection container added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kylipp/config/theme/theme.dart';
 import 'package:kylipp/features/authentication/presentation/screens/log_in_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'injection_container.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await initializeDependencies();
+
   runApp(const KYLIPP());
 }
+
+class FirebaseAppCheck {}
 
 class KYLIPP extends StatelessWidget {
   const KYLIPP({super.key});
