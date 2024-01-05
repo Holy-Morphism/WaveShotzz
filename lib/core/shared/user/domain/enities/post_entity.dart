3dfc2d6e2a484f5cd2e0f8adfe6e52fcc5b81f59
Holy-Morphism <ranaadeeltahir@gmail.com>
2024-01-06 00:14:07 +0500

    post entity added

---
+++ lib/core/shared/user/domain/enities/post_entity.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+import 'comment_entity.dart';
+
+class PostEntity extends Equatable {
+  final String url;
+  final String caption;
+  final DateTime date;
+  final int likes;
+  final List<CommentEntity> comments;
+  final bool isVideo;
+  const PostEntity({
+    required this.url,
+    required this.caption,
+    required this.date,
+    required this.likes,
+    this.isVideo = false,
+    required this.comments,
+  });
+
+  @override
+  List<Object> get props => [url, caption, date, likes, comments, isVideo];
+}
