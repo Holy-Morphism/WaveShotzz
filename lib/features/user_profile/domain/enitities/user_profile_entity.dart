2390198a4c3f0cfc4871311a296ec6e0a11b76d3
Holy-Morphism <ranaadeeltahir@gmail.com>
2024-01-06 00:15:07 +0500

    domain layer done

--- lib/features/user_profile/domain/enitities/user_profile_entity.dart
+++ lib/features/user_profile/domain/enitities/user_profile_entity.dart
@@ -1,10 +1,21 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/widgets.dart';
 
-class UserProfileEntity {
+import '../../../../core/shared/user/domain/enities/post_entity.dart';
+
+class UserProfileEntity extends Equatable {
   final String name;
-  final NetworkImage image;
-  final String bio;
-  final int followers;
-  final int following;
+  final String email;
+  final MemoryImage profilePicture;
   final List<PostEntity> posts;
+
+  const UserProfileEntity({
+    required this.name,
+    required this.email,
+    required this.profilePicture,
+    required this.posts,
+  });
+
+  @override
+  List<Object> get props => [name, email, profilePicture, posts];
 }
