cff89c9e60bc425688953a6766e99d5a451b627f
Holy-Morphism <ranaadeeltahir@gmail.com>
2024-04-07 19:59:37 +0500

    name changed

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,5 +1,5 @@
 <!-- omit in toc -->
-# Contributing to KYLIPP
+# Contributing to WAVESHOTZZ
 
 First off, thanks for taking the time to contribute! ❤️
 
--- lib/features/explore/domain/repository/explore_repository.dart
+++ lib/features/explore/domain/repository/explore_repository.dart
@@ -1,15 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:waveshotzz/core/shared/user/domain/entities/comment_entity.dart';
-import 'package:waveshotzz/features/explore/domain/enitties/post_entity.dart';
+import 'package:waveshotzz/features/explore/domain/entities/post_entity.dart';
 
 import '../../../../core/error/failure.dart';
 
 abstract class ExploreRepository {
   Future<Either<Failure, List<PostEntity>>> getExplorePosts();
-  Future<Either<Failure,void>> likePosts({required String postId});
-  Future<Either<Failure,void>> unlikePosts({required String postId});
+  Future<Either<Failure,void>> likePost({required String postId});
+  Future<Either<Failure,void>> unlikePost({required String postId});
   Future<Either<Failure,List<CommentEntity>>> getCommentsOnPost({required String postId});
-  Future<Either<Failure,void>> commentOnPosts({required String postId,required String comment});
+  Future<Either<Failure,void>> commentOnPost({required String postId,required String comment});
   Stream<Either<Failure,List>> getUserSearchResults();
   
 }
