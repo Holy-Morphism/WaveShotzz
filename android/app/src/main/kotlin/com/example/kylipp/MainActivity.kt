1bcac51d0e09b2db84c1ece5f01432f255338477
Holy-Morphism <ranaadeeltahir@gmail.com>
2024-05-25 17:56:19 +0500

    legacy code removed

---
+++ android/app/src/main/kotlin/com/example/kylipp/MainActivity.kt
@@ -0,0 +1,6 @@
+package untameddevelopment.kylippv2.com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/waveshotzz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.holymorphism.waveshotzz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
